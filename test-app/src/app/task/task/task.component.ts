004a4d9da1d3cffc47262f35480c26ae3e57b93f
Apurba Hasan <apurbahasan1994@gmail.com>
2021-11-24 13:27:05 +0600

    corrected navigation error

--- test-app/src/app/task/task/task.component.ts
+++ test-app/src/app/task/task/task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
+const tasks_array = ["Seo", "Link Building", "Angular functionality improve"];
 
 @Component({
   selector: 'app-task',
@@ -12,7 +13,8 @@ export class TaskComponent implements OnInit {
   taskFrom = new FormGroup({
     task: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern(/[A-Z]+$/i)])
   })
-  tasks: string[] = ["Seo", "Link Building", "Angular functionality improve"];
+  tasks = Array.from(tasks_array)
+
   constructor() {
 
   }
@@ -26,6 +28,8 @@ export class TaskComponent implements OnInit {
     console.log(this.taskFrom.value)
     const newtask: string = this.taskFrom.get('task')?.value
     this.tasks.push(newtask)
+    tasks_array.push(newtask)
+
     this.taskFrom.reset();
   }
 
